afc34236657b17648c31b8f22b2539232b7c96f2
Marti <52050778+github-marti@users.noreply.github.com>
2020-03-05 03:11:07 -0800

    initializing firebase.js config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 const firebaseConfig = {
     apiKey: 'AIzaSyDuyOgBSGSbXtnp5I7cBXBeMmfTPnWp7S8',
     authDomain: 'martis-bug-tracker.firebaseapp.com',
@@ -8,3 +12,7 @@ const firebaseConfig = {
     appId: '1:102685929723:web:b9a9d8d36b9da1d19e805e',
     measurementId: 'G-P1PNN109W6',
 };
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;

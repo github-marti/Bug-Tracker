289ea92f492861d0dd5eda2041f917f448e12195
Marti <52050778+github-marti@users.noreply.github.com>
2020-03-05 03:06:16 -0800

    imitialized firebaseconfig

---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyDuyOgBSGSbXtnp5I7cBXBeMmfTPnWp7S8',
+    authDomain: 'martis-bug-tracker.firebaseapp.com',
+    databaseURL: 'https://martis-bug-tracker.firebaseio.com',
+    projectId: 'martis-bug-tracker',
+    storageBucket: 'martis-bug-tracker.appspot.com',
+    messagingSenderId: '102685929723',
+    appId: '1:102685929723:web:b9a9d8d36b9da1d19e805e',
+    measurementId: 'G-P1PNN109W6',
+};

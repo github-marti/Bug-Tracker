46b92fd6cc33ed5b8663dae1478d46b883b5ba04
Marti <52050778+github-marti@users.noreply.github.com>
2020-03-05 23:25:25 -0800

    created actions.ts in types

---
+++ src/types/actions.ts
@@ -0,0 +1,47 @@
+import { User } from '../types/User';
+
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
+
+export const VERIFY_REQUEST = 'VERIFY_REQUEST';
+export const VERIFY_SUCCESS = 'VERIFY_SUCCESS';
+
+export interface RequestLoginAction {
+    type: typeof LOGIN_REQUEST;
+}
+
+export interface ReceiveLoginAction {
+    type: typeof LOGIN_SUCCESS;
+    user: User;
+}
+
+export interface LoginFailureAction {
+    type: typeof LOGIN_FAILURE;
+    error: Error;
+}
+
+export interface RequestLogoutAction {
+    type: typeof LOGOUT_REQUEST;
+}
+
+export interface ReceiveLogoutAction {
+    type: typeof LOGOUT_SUCCESS;
+}
+
+export interface LogoutFailureAction {
+    type: typeof LOGOUT_FAILURE;
+    error: Error;
+}
+
+export interface VerifyRequestAction {
+    type: typeof VERIFY_REQUEST;
+}
+
+export interface VerifySuccessAction {
+    type: typeof VERIFY_SUCCESS;
+}

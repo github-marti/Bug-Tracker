a46d0d1a9a2c81089e845cd47d22b657d1cd7239
Marti <52050778+github-marti@users.noreply.github.com>
2020-03-05 23:44:01 -0800

    moved configurestore and typescripted out actions/auth

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,64 +1,55 @@
 import { myFirebase } from '../firebase/firebase';
-
-export const LOGIN_REQUEST = 'LOGIN_REQUEST';
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-export const LOGIN_FAILURE = 'LOGIN_FAILURE';
-
-export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
-export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
-export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
-
-export const VERIFY_REQUEST = 'VERIFY_REQUEST';
-export const VERIFY_SUCCESS = 'VERIFY_SUCCESS';
+import { User } from '../types/User';
+import { AppActions } from '../types/actions';
 
 const requestLogin = () => {
     return {
-        type: LOGIN_REQUEST,
+        type: 'LOGIN_REQUEST',
     };
 };
 
-const receiveLogin = user => {
+const receiveLogin = (user: User): AppActions => {
     return {
-        type: LOGIN_SUCCESS,
+        type: 'LOGIN_SUCCESS',
         user,
     };
 };
 
-const loginFailure = error => {
+const loginFailure = (error: Error): AppActions => {
     return {
-        type: LOGIN_FAILURE,
+        type: 'LOGIN_FAILURE',
         error,
     };
 };
 
 const requestLogout = () => {
     return {
-        type: LOGOUT_REQUEST,
+        type: 'LOGOUT_REQUEST',
     };
 };
 
 const receiveLogout = () => {
     return {
-        type: LOGOUT_SUCCESS,
+        type: 'LOGOUT_SUCCESS',
     };
 };
 
-const logoutFailure = error => {
+const logoutFailure = (error: Error): AppActions => {
     return {
-        type: LOGOUT_FAILURE,
+        type: 'LOGOUT_FAILURE',
         error,
     };
 };
 
 const verifyRequest = () => {
     return {
-        type: VERIFY_REQUEST,
+        type: 'VERIFY_REQUEST',
     };
 };
 
 const verifySuccess = () => {
     return {
-        type: VERIFY_SUCCESS,
+        type: 'VERIFY_SUCCESS',
     };
 };
 
--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,10 +1,11 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
+import { verifyAuth } from '../actions';
+import rootReducer from '../reducers';
 
-import { verifyAuth } from './actions/';
-import rootReducer from './reducers/';
+export type AppState = ReturnType<typeof rootReducer>;
 
-export default function configureStore(persistedState) {
+export default function configureStore(persistedState: any) {
     const store = createStore(rootReducer, persistedState, applyMiddleware(thunkMiddleware));
     store.dispatch(verifyAuth());
     return store;

3e1c71642cb779e346f8c4f445f43d50634a097c
Marti <52050778+github-marti@users.noreply.github.com>
2020-03-06 00:39:31 -0800

    added thunkmiddleware to configurestore

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,12 +1,17 @@
 import { applyMiddleware, createStore } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import thunk, { ThunkMiddleware } from 'redux-thunk';
 import { verifyAuth } from '../actions';
 import rootReducer from '../reducers';
+import { AppActions } from '../types/actions';
 
 export type AppState = ReturnType<typeof rootReducer>;
 
 export default function configureStore(persistedState: any) {
-    const store = createStore(rootReducer, persistedState, applyMiddleware(thunkMiddleware));
+    const store = createStore(
+        rootReducer,
+        persistedState,
+        applyMiddleware(thunk as ThunkMiddleware<AppState, AppActions>),
+    );
     store.dispatch(verifyAuth());
     return store;
 }
